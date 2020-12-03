a3de47c361b4e03c27d5eddf994fdac66b51a34b
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-12-03 19:59:46 -0300

    aspas

---
+++ 05-aspas_contatemacao.php
@@ -0,0 +1,9 @@
+<?php
+//Aspas simples são literais, não sao intrepretadas
+$nome = 'Felipe';
+echo 'Meu nome é '.$nome. ' e minha idade é \'21\'';
+//use \'2\' para escrever emtre aspas
+
+echo "<hr>";
+echo "Meu nome é \"$nome\""; // para variaveis tambem é possivel utilizar aspas 
+
