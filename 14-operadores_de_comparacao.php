79db39f4e2e1d736c57350f2451dc0be283624f3
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-12-04 19:26:07 -0300

    Create 14-operadores_de_comparacao.php

---
+++ 14-operadores_de_comparacao.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+Operadores de comparação
+
+==      iqualdade
+!=      não iqual
+===     indentidade (identico ao outro)
+!===    não identico
+<>      diferente
+<       nenor que
+>       maior que
+<=      nenor ou iqual
+>=      maior ou iqual
+<=>     spaceship (se o lado esquerdo for maior ele retorna -1, se forem iquais retorna 0 e se o lado direito for maior ele retorna 1)
+
+*/
+
+if (10 == 10) {
+	echo "positivo";
+} else {
+	echo "negativo";
+}
