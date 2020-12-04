7ad7698f8f96bbd800673c1b0c56d491f53fd937
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2020-12-04 19:01:25 -0300

    Create 11-operadores_aritimeticos.php

---
+++ 11-operadores_aritimeticos.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+Operadores aritimeticos
+
+São usados com valores numericos para executar operações
+
+Adição +
+Subtração -
+Multiplicação *
+Divisão /
+Módulo %
+Exponenciação **
+*/
+
+echo 10 + 10;
+echo 14 - 6;
+echo 4 * 2;
+echo 20 / 2;
+echo 50 % 5;
+echo 2 ** 5;
+
+echo "<hr>";
+
+$a = 10;
+$b = 20;
+$c = 4;
+$d = 8;
+
+$adicao = $a + $b;
+echo $adicao;
+
+echo "<hr>";
+//definir prioridade com o ()
+
+echo (3 + 4 + 6 + 5) / 4;
